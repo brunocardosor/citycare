f35c08bb9d5e3cabda817268e9dadd71fc6838c4
plsb <pedroluissaraiva2@hotmail.com>
2016-11-25 19:39:20 -0300

    deleta denúncia

--- CityCare/src/main/java/com/citycare/controller/DenunciaController.java
+++ CityCare/src/main/java/com/citycare/controller/DenunciaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,14 +18,14 @@ import com.citycare.model.UsuarioSingleton;
 
 @Controller
 public class DenunciaController {
-	@Autowired //injeção de dependencia 
+	@Autowired // injeção de dependencia
 	private DenunciaRepository dr;
-	
+
 	@Autowired
 	private CategoriaRepository cr;
-	
-	@RequestMapping(value="feed")
-	public ModelAndView feedDenuncias(){
+
+	@RequestMapping(value = "feed")
+	public ModelAndView feedDenuncias() {
 		Usuario usuario = UsuarioSingleton.getInstance();
 		List<Categoria> categoria = cr.findAll();
 		List<Denuncia> denuncia = dr.findAllByOrderByIdDesc();
@@ -32,13 +33,13 @@ public class DenunciaController {
 		ModelAndView mv = new ModelAndView("/denuncia/feed-denuncias");
 		mv.addObject("nomeUsuario", usuario.getNome());
 		mv.addObject("qntdDenuncias", qntdDenuncia.size());
-		mv.addObject("todosValoresCategoria",categoria);
+		mv.addObject("todosValoresCategoria", categoria);
 		mv.addObject("todosValoresDenuncia", denuncia);
-		return mv;	
+		return mv;
 	}
-		
-		@RequestMapping(value="profile")
-		public ModelAndView profileUsuario(Usuario usuario){
+
+	@RequestMapping(value = "profile")
+	public ModelAndView profileUsuario(Usuario usuario) {
 		usuario = UsuarioSingleton.getInstance();
 		List<Denuncia> denuncia = dr.findByUsuarioOrderByIdDesc(usuario);
 		ModelAndView mv = new ModelAndView("/usuario/profile");
@@ -47,33 +48,33 @@ public class DenunciaController {
 		mv.addObject("todosValoresDenuncia", denuncia);
 		return mv;
 	}
-	
-	@RequestMapping(value="adicionaDenuncia")
-	public ModelAndView adicionaDenuncia(Denuncia denuncia){
+
+	@RequestMapping(value = "adicionaDenuncia")
+	public ModelAndView adicionaDenuncia(Denuncia denuncia) {
 		denuncia.setUsuario(UsuarioSingleton.getInstance());
 		dr.save(denuncia);
 		return feedDenuncias();
 	}
-	
-	@RequestMapping(value="deletaDenuncia")
-	public ModelAndView deletaDenuncia(Denuncia denuncia){
+
+	@RequestMapping(value = "deletaDenuncia/{codigo}")
+	public ModelAndView deletaDenuncia(@PathVariable("codigo") Denuncia denuncia) {
 		dr.delete(denuncia);
 		Usuario usuario = UsuarioSingleton.getInstance();
 		return profileUsuario(usuario);
 	}
-	
-	@RequestMapping(value="pesquisar")
-	public ModelAndView pesquisar(String descricao){
+
+	@RequestMapping(value = "pesquisar")
+	public ModelAndView pesquisar(String descricao) {
 		ModelAndView mv = new ModelAndView("/denuncia/pesquisa");
-		List<Categoria> categoria = cr.findByDescricaoContaining(descricao);	
+		List<Categoria> categoria = cr.findByDescricaoContaining(descricao);
 		ArrayList<Denuncia> todasDenuncias = new ArrayList<Denuncia>();
-		for(Categoria c : categoria){
+		for (Categoria c : categoria) {
 			List<Denuncia> denuncia = dr.findByCategoriaOrderByIdDesc(c);
-			for(Denuncia d : denuncia){
+			for (Denuncia d : denuncia) {
 				todasDenuncias.add(d);
 			}
 		}
-		
+
 		Usuario usuario = UsuarioSingleton.getInstance();
 		List<Denuncia> qntdDenuncia = dr.findByUsuarioOrderByIdDesc(usuario);
 		mv.addObject("resultadoPesquisa", todasDenuncias);
